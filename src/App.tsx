cbae640f08329b5dc3f73aaf7def3c28efc60865
zonetecde <zonetecde@gmail.com>
2023-09-30 11:33:00 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-2xl">Welcome to Tauri!</h1>
+    </div>
+  );
+}
+
+export default App;
