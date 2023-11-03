3719feeab4fea863f354ec059f63bb2b1f8117af
Rayane Staszewski <zonetecde@gmail.com>
2023-11-03 17:15:17 +0100

    added more space between words with me_quran font

--- src/extensions/subtitleGenerator.ts
+++ src/extensions/subtitleGenerator.ts
@@ -77,12 +77,14 @@ Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\
                     (font !== "me_quran" ? "\\h" : "") + // espace entre le nbre arabe et le texte (uniquement pour Amiri car me_quran ne supporte pas les espaces)
                     this.setFontSizeExpression(arabicFontSize) +
                     this.setFontExpression(font) +
-                    subtitle.getArabicText(
-                        arabicVersesBetweenParentheses,
-                        false,
-                        true,
-                        font
-                    ) +
+                    subtitle
+                        .getArabicText(
+                            arabicVersesBetweenParentheses,
+                            false,
+                            true,
+                            font
+                        )
+                        .replaceAll(" ", font === "me_quran" ? "     " : " ") +
                     (secondLang === "none" || subtitle.versePos === undefined // si on veut la traduction avec et que ce n'est pas une basmala ou autre
                         ? ""
                         : this.NEW_SUBTITLE_LINE + // la trad est sur une autre ligne
